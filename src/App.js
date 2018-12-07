b9890bcf3cb70cc3002874028ec6138a30d01ff4
Daniel Olagunju <todak2000@gmail.com>
2018-12-07 10:08:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import Icon from '@material-ui/core/Icon';
+import a2b from '../src/img/a2b100.png';
+
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    // this.state = { profile_pix: [] };
+    
+    this.state = {
+      first_name:'',
+      last_name:'',
+      email : '' ,
+      phone_no : '',
+      address:'',
+      //profile_pix:['.jpg','.png', '.gif','.jpeg','.svg']
+      
+      
+      
+    };
+    //this.onDrop = this.onDrop.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+// onDrop(picture) {
+//     this.setState({
+//         profile_pix: this.state.profile_pix.concat(picture),
+//     });
+// }
+
+handleChange(event){
+        this.setState({
+            [event.target.name] : event.target.value
+            
+        });
+}
+handleSubmit(event){
+        event.preventDefault();
+
+        const user = {
+        first_name:this.state.first_name, 
+        last_name:this.state.last_name,
+        phone_no :this.state.phone_no,
+        email : this.state.email,
+        address:this.state.address,
+        //profile_pix:this.state.profile_pix
+        
+        
+        };
+        console.log(user);
+  axios.post('http://192.168.10.201/a2b_api/public/register',user,{
+})
+//   .catch((error) => alert("User Already exists"))
+//   .catch((message)=>alert("Successfully Registered"))
+  
+  .then(function (Response) {
+     console.log(Response);
+    })
+  .catch(function (error) {
+    console.log(error);
+    
+  });
+        
+
+
+
+}
+
+  render() {
+  
+    return (
+        
+        <div>
+               
+            <form className="form-horizontal" ref="form" onSubmit={this.handleSubmit}>
+            <img src={a2b} alt="logo" /><h3 style={{  color:'rgb(255,255,255)' }}>Registration Portal</h3>
+            <div className="form-group">
+                <div className="col-sm-2 icona">
+                    <Icon  style={{ fontSize: 34, color:'rgb(55,76,199)' }}>person</Icon>
+                </div>
+                <div className="col-sm-10">
+                    <input type="first_name" name="first_name" onChange={this.handleChange} className="form-control" id="exampleInputEmail1" placeholder="FirstName" required />
+                </div>
+            </div>
+
+            <div className="form-group">
+                <div className="col-sm-2 icona">
+                    <Icon  style={{ fontSize: 34, color:'rgb(55,76,199)' }}>person</Icon>
+                </div>
+                <div className="col-sm-10">
+                    <input type="last_name" name="last_name" onChange={this.handleChange} className="form-control" id="exampleInputEmail1" placeholder="Last Name" required />
+                </div>
+            </div>
+            
+            
+            <div className="form-group">
+                <div className="col-sm-2 icona">
+                    <Icon  style={{ fontSize: 34, color:'rgb(55,76,199)' }}>email</Icon>
+                </div>
+                <div className="col-sm-10">
+                    <input type="email" name="email" onChange={this.handleChange} className="form-control" id="exampleInputEmail1" placeholder="Email" required />
+                </div>
+            </div>
+                <div className="form-group">
+                <div className="col-sm-2 icona">
+                    <Icon  style={{ fontSize: 34, color:'rgb(55,76,199)'}}>phone</Icon>
+                </div>
+                <div className="col-sm-10">
+                    <input type="phone_no" name="phone_no" onChange={this.handleChange} className="form-control" id="exampleInputPassword1" placeholder="Phone Number" required/>
+                </div>
+            </div>
+            <div className="form-group">
+                <div className="col-sm-2 icona">
+                    <Icon  style={{ fontSize: 34, color:'rgb(55,76,199)' }}>place</Icon>
+                </div>
+                <div className="col-sm-10">
+                    <input type="address" name="address" onChange={this.handleChange} className="form-control" id="exampleInputEmail1" placeholder="Address" required />
+                </div>
+            </div>
+            
+            {/* <div className="form-group">
+                <div className="col-sm-10">
+                    <ImageUploader
+                        withIcon={true}
+                        buttonText='Choose images'
+                        onChange={this.onDrop}
+                        imgExtension={['.jpg','.png', '.gif','.jpeg','.svg']}
+                        maxFileSize={5242880}
+                    />
+
+                    </div>
+            </div>
+            */}
+            <br/>
+            <div className="form-group">    
+                <div className="butt">
+                
+                    <button type="submit" className="but_sub">
+                    <Icon  style={{ fontSize: 34, color:'rgb(55,76,199)' }}>send</Icon>
+                    
+                    </button>
+                </div>
+            </div>
+        </form>
+</div>
+    );
+  }
+}
+
+export default App;
